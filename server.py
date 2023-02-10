1f60697ed83e26fe97a72cd9fb4bc7d3f17fea2d
vuonglv <it.vuonglv@gmail.com>
2023-02-10 17:53:08 +0700

    fixed

--- server.py
+++ server.py
@@ -3,4 +3,4 @@ import os
 import uvicorn
 
 if __name__ == "__main__":
-    uvicorn.run("api:app", host="0.0.0.0", port=os.getenv("PORT", 8000))
+    uvicorn.run("api:app", host="0.0.0.0", port=int(os.getenv("PORT", 8000)))
